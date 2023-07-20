3b161b21d17d02b751c400106d00a865f9d0d15b
Long Le <vietlong.lenguyen@gmail.com>
2023-07-20 00:34:38 -0400

    desktop sync

--- my_code/train_docker.sh
+++ my_code/train_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 srun --container-image=/home/vlongle/code/ManiSkill2/lnle+mani_skill2+latest.sqsh\
- --mounts=/home/vlongle/code/ManiSkill2/:/ManiSkill2/my_code\
+ --container-mounts=/home/vlongle/code/ManiSkill2/my_code:/ManiSkill2/my_code\
  --gpus=1\
  --nodes=1\
  --cpus-per-gpu=24\
