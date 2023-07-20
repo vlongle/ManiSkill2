ea50cca73ecf6fec74d5c5777a8385d2fe69c1a1
Long Le <vietlong.lenguyen@gmail.com>
2023-07-20 01:11:29 -0400

    train more

---
+++ my_code/reduced_env_ids.py
@@ -0,0 +1,7 @@
+env_ids = [
+    "PickCube-v0",
+    "StackCube-v0",
+    "PickSingleYCB-v0",
+    "PickSingleEGAD-v0",
+    "PickClutterYCB-v0",
+]
--- my_code/train_docker.sh
+++ my_code/train_docker.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 srun --container-image=/home/vlongle/code/ManiSkill2/lnle+mani_skill2+latest.sqsh\
  --container-mounts=/home/vlongle/code/ManiSkill2/my_code:/ManiSkill2/my_code\
- --gpus=1\
+ --gpus=4\
  --nodes=1\
  --cpus-per-gpu=24\
  --mem-per-cpu=1G\
  --time=72:00:00\
  --qos=ee-med\
  --partition=eaton-compute \
-bash test_err.sh
+bash -c "unset DISPLAY && python train.py / 2>&1 | tee log.txt"
 
 exit 3
